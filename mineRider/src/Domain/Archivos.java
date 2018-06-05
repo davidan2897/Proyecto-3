b8fe7e62f8db1f8d8d12bd7a3fddad31ed1cd1ad
davidan2897 <37605944+davidan2897@users.noreply.github.com>
2018-06-05 17:17:10 -0600

    archivo

--- mineRider/src/Domain/Archivos.java
+++ mineRider/src/Domain/Archivos.java
@@ -4,11 +4,53 @@
  * and open the template in the editor.
  */
 package Domain;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 /**
  *
  * @author David
  */
 public class Archivos {
-    
+
+
+
+public void EscribirJson(){
+
+        JSONParser parser = new JSONParser();
+
+        try {     
+            Object obj = parser.parse(new FileReader("c:\\file.json"));
+
+            JSONObject jsonObject =  (JSONObject) obj;
+
+            String name = (String) jsonObject.get("name");
+            System.out.println(name);
+
+            String city = (String) jsonObject.get("city");
+            System.out.println(city);
+
+            String job = (String) jsonObject.get("job");
+            System.out.println(job);
+
+            // loop array
+            JSONArray cars = (JSONArray) jsonObject.get("cars");
+            Iterator<String> iterator = cars.iterator();
+            while (iterator.hasNext()) {
+             System.out.println(iterator.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
