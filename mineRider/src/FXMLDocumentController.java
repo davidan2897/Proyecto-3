9fe61a7a8b395c115a6918c35682fcf31cbee525
davidan2897 <37605944+davidan2897@users.noreply.github.com>
2018-06-05 19:40:14 -0600

    Interfaz

--- mineRider/src/FXMLDocumentController.java
+++ mineRider/src/FXMLDocumentController.java
@@ -1,20 +1,16 @@
 
-import Domain.Archivos;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javax.swing.JOptionPane;
 /**
  *
- * @author david
+ * @author David
  */
 public class FXMLDocumentController implements Initializable {
 
